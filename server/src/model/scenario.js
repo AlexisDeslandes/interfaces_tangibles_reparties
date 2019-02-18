22877730c20c8c6858385a40173101398424a899
NastyJoke <renaud.costa84@gmail.com>
2019-02-18 14:31:58 +0100

    Intégration mini jeu dans le scénar

--- server/src/model/scenario.js
+++ server/src/model/scenario.js
@@ -25,7 +25,7 @@ module.exports = [
                 result: "Vous tentez d'emporter un parasol. Malheureusement, vous ne parvenez pas à le faire rentrer" +
                     "dans votre sacoche...",
                 stats: [
-                    {type: 'mood', value: -1},
+                    {type: 'mood', value: -10},
                 ]
             },
             {
@@ -302,7 +302,8 @@ module.exports = [
                 text: "Vous avez des problèmes je crois",
                 result: "Vous parvenez à calmer les touristes et vous les aidez à se sortir de leurs ennuis. Ils vous " +
                     "remercient chaleureusement, et vous partez chacun de votre côté. Cette interaction humaine vous " +
-                    "redonne le sourire.",
+                    "redonne le sourire. Vous partez maintenant pour Gao mais voila que vous êtes pris dans une tempête " +
+                "de sable !",
                 stats : [
                     {type : "mood", value : 1}
                 ]
@@ -311,12 +312,16 @@ module.exports = [
                 text: "Vous venez souvent par ici ?",
                 result: "Vous parvenez à calmer les touristes et vous les aidez à se sortir de leurs ennuis. Ils vous " +
                     "remercient chaleureusement, et vous partez chacun de votre côté. Cette interaction humaine vous " +
-                    "redonne le sourire.",
+                    "redonne le sourire. Vous partez maintenant pour Gao mais voila que vous êtes pris dans une tempête " +
+                "de sable !",
                 stats : [
                     {type : "mood", value : 1}
                 ]
             }
         ]
+    },
+    {
+        type: "minijeu"
     },
     {
         intro: [{
@@ -380,8 +385,5 @@ module.exports = [
                 ]
             },
         ]
-    },
-    {
-        type : "minijeu"
     }
 ];
